edd7a62e25fe4449e76e1982961fe13fcd8113ab
mateo <160982740+mateoperezspycloud@users.noreply.github.com>
2025-05-26 12:19:24 -0500

    2025 release

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,31 @@
-import { AccordionList,About, Contact, Footer, Landing, Navbar, TimeLine, Projects } from "@/components";
+import {
+  AccordionList,
+  About,
+  Contact,
+  Footer,
+  Landing,
+  Navbar,
+  TimeLine,
+  Projects,
+} from "@/components";
 import { TimeLineProps } from "@/components/TimeLine";
 import { VStack } from "@chakra-ui/react";
 import { AccordionListProps } from "@/components/Accordion";
 import { LandingProps } from "@/components/Landing";
 import { FooterProps } from "@/components/Footer";
 import { NavbarProps } from "@/components/Navbar";
+import { ProjectsProps } from "@/components/Projects";
 type Body = {
   landing: LandingProps;
+  projects: ProjectsProps;
   accordion: AccordionListProps;
   timeline: TimeLineProps;
-}
+};
 type Data = {
   navbar: NavbarProps;
   body: Body;
   footer: FooterProps;
-}
+};
 const data: Data = {
   navbar: {
     logo: "cmp",
@@ -29,7 +40,43 @@ const data: Data = {
     landing: {
       message:
         "hello! im mateo pérez. senior software engineer, reader & investor",
-      image: "/next.svg",
+      image: "/me.png",
+    },
+    projects: {
+      title: "featured projects",
+      subtitle:
+        "here are some of the projects i've worked on. each project represents a unique challenge and learning opportunity.",
+      items: [
+        {
+          title: "spycloud",
+          description:
+            "investigations platform to track and check the exposure of companies leacked data. we build from  jupyter notebooks to a production ready platform serving +250 companies like amex, paypal, aws & goverments",
+          domain: "https://spycloud.com",
+          technologies: ["react", "auth0", "go", "terraform", "docker", "aws"],
+        },
+        {
+          title: "blossom",
+          description:
+            "core banking system & cms to manage credit unions operations & content in usa. started as custom software for 5 credit unions in 2020, now serving +350 with a baas model",
+          domain: "https://blossom.net",
+          technologies: ["next", "graphql", "postgres", "docker", "aws"],
+        },
+        {
+          title: "compleat",
+          description:
+            "personal project which i worked on with 2 more teammates. our goal was to digitize all restaurant operations in Bogota and Latam.We applied to the yc-2020 Winter batch and got to the second round.",
+          domain: "https://compleat.com.co",
+          image: "/compleat.jpeg",
+          technologies: ["next", "django", "auth0", "postgres", "aws"],
+        },
+        {
+          title: "auxo",
+          description:
+            "developed e2e features for multiple fintech products and kyc services, ensuring seamless integration and compliance throughout the user journey. using react, django & auth0.",
+          domain: "https://auxo.co",
+          technologies: ["next", "django", "auth0", "postgres", "aws"],
+        },
+      ],
     },
     accordion: {
       title: "book list 📚",
@@ -117,47 +164,51 @@ const data: Data = {
     },
     timeline: {
       title: "experience",
+      size: "xl",
       items: [
         {
           type: "work",
-          period: ["2025-now"],
-          description:
-            "sr fullstack developer.",
+          title: "praxent",
+          period: ["2023-now"],
+          children: {
+            size: "lg",
+            items: [
+              {
+                type: "growth",
+                period: ["2025-now"],
+                description: "sr fullstack developer.",
+              },
+              {
+                type: "startpoint",
+                period: ["2023-2025"],
+                description: "mid fullstack developer.",
+              },
+            ],
+          },
         },
         {
           type: "work",
-          title: "Praxent",
-          period: ["2023-2025"],
-          description:
-            "mid fullstack developer.",
-        },
-        {
-          type: "work",
-          title: "Auxo",
+          title: "auxo",
           period: ["2022-2022"],
-          description:
-            "mid fullstack developer.",
+          description: "mid fullstack developer.",
         },
         {
           type: "work",
-          title: "HelloBuild",
+          title: "hellobuild",
           period: ["2021-2022"],
-          description:
-            "mid fullstack developer.",
+          description: "mid fullstack developer.",
         },
         {
           type: "project",
           title: "compleat",
           period: ["2021-2022"],
-          description:
-            "co-founder & jr fullstack developer.",
+          description: "co-founder & jr fullstack developer.",
         },
         {
           type: "work",
-          title: "Amcon",
+          title: "amcon",
           period: ["2020-2021"],
-          description:
-            "jr fronted developer.",
+          description: "jr fronted developer.",
         },
         {
           type: "education",
@@ -202,34 +253,29 @@ const data: Data = {
 };
 export default function Home() {
   const { navbar, body, footer } = data;
-  const { timeline, accordion, landing } = body;
+  const { timeline, accordion, landing, projects } = body;
   return (
-
-      <VStack
-        margin={"auto"}
-        maxW={{ sm: "90%", md: "75%", "2xl": 1280 }}
-         w={{base:'100%',sm:'100%',md:'75%'}}
+    <VStack
+      margin={"auto"}
+      maxW={{ sm: "90%", md: "75%", "2xl": 1280 }}
+      w={{ base: "100%", sm: "100%", md: "75%" }}
       m="auto"
-        justifyContent="space-between"
-        alignItems="center"
-        minHeight="100vh"
-        gap={20}
-        px={{base:4,sm:0,md:0}}
-      >
-        <Navbar {...navbar} />
+      justifyContent="space-between"
+      alignItems="center"
+      minHeight="100vh"
+      gap={20}
+      px={{ base: 4, sm: 0, md: 0 }}
+    >
+      <Navbar {...navbar} />
 
+      <Landing {...landing} />
+      <About />
+      <Projects {...projects} />
+      <TimeLine {...timeline} />
+      <AccordionList {...accordion} />
 
-          <Landing {...landing}/>
-          <About />
-          <Projects />
-          <TimeLine {...timeline} />
-          <AccordionList {...accordion} />
-          
-          <Contact />
-          <Footer {...footer} />
-      </VStack>
-
-     
-
+      <Contact />
+      <Footer {...footer} />
+    </VStack>
   );
 }
--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -10,59 +10,31 @@ import {
   VStack,
 } from "@chakra-ui/react";
 
-const projects = {
-  title: "featured projects",
-  items: [
-    {
-      title: "spycloud",
-      description:
-        "investigations platform to track and check the exposure of companies leacked data. we build from  jupyter notebooks to a production ready platform serving +250 companies like amex, paypal, aws & goverments",
-      domain: "https://spycloud.com",
-      technologies: ["react", "auth0", "go", "terraform", "docker", "aws"],
-      featured: true,
-    },
-    {
-      title: "blossom",
-      description:
-        "core banking system & cms to manage credit unions operations & content in usa. started as custom software for 5 credit unions in 2020, now serving +350 with a baas model",
-      domain: "https://blossom.net",
-      technologies: ["next", "graphql", "postgres", "docker", "aws"],
-      featured: true,
-    },
-    {
-      title: "compleat",
-      description:
-        "personal project which i worked on with 2 more teammates. our goal was to digitize all restaurant operations in Bogota and Latam.We applied to the yc-2020 Winter batch and got to the second round.",
-      domain: "https://compleat.com.co",
-      image:
-        "https://media.licdn.com/dms/image/v2/D4E2DAQF_CqJyC8BZyA/profile-treasury-image-shrink_1920_1920/B4EZbLmSKDHcAg-/0/1747172533212?e=1747857600&v=beta&t=r2iwVslfeesqA3TdWtLH7VmFJS1SWGynixg4hwqy8AA",
-      technologies: ["next", "django", "auth0", "postgres", "aws"],
-      featured: false,
-    },
-    {
-      title: "auxo",
-      description:
-        "developed e2e features for multiple fintech products and kyc services, ensuring seamless integration and compliance throughout the user journey. using react, django & auth0.",
-      domain: "https://auxo.co",
-      technologies: ["next", "django", "auth0", "postgres", "aws"],
-      featured: false,
-    },
-  ],
+type Project = {
+  title: string;
+  description: string;
+  domain: string;
+  technologies: string[];
+  image?: string;
+};
+export type ProjectsProps = {
+  title: string;
+  subtitle: string;
+  items: Project[];
 };
 
-const Projects = () => {
+const Projects = ({ title, subtitle, items }: ProjectsProps) => {
   return (
     <VStack align="stretch" id="projects">
       <Heading size="xl" textAlign="center">
-        {projects.title}
+        {title}
       </Heading>
       <Text textAlign="center" color="gray.500" maxW="2xl" mx="auto">
-        here are some of the projects i&apos;ve worked on. each project represents a
-        unique challenge and learning opportunity.
+        {subtitle}
       </Text>
 
       <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} gap={8} mt={8}>
-        {projects.items.map(async (project, index) => {
+        {items.map(async (project, index) => {
           const logo = await getDomainLogo(project.domain, "medium");
           return (
             <Box
@@ -85,7 +57,7 @@ const Projects = () => {
                 <Heading as="h3" size="md" mb={2}>
                   {project.title}
                 </Heading>
-                <Text color="gray.600" mb={4}>
+                <Text color="gray.500" mb={4}>
                   {project.description}
                 </Text>
 
